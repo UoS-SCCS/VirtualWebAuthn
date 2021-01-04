e06e611ccd9c23af59b10db99decf9ce1c12fc7b
Chris Culnane <chrisculnane@gmail.com>
2021-01-04 11:56:40 +0000

    Fix  to only create data directory if it is missing and not just if the storage file is missing

--- src/authenticator/json_storage.py
+++ src/authenticator/json_storage.py
@@ -47,8 +47,8 @@ class JSONAuthenticatorStorage(DICEAuthenticatorStorage):
         super().__init__()
         self._path = path
         if not self._check_exists():
-
-            os.mkdir(os.path.dirname(self._path))
+            if not os.path.exists(os.path.dirname(self._path)):
+                os.mkdir(os.path.dirname(self._path))
             self._data={"_version":"JSONAuthenticatorStorage_0.1"}
             self._data[STORAGE_KEYS.CREDENTIALS]={}
             self._write_to_json()
